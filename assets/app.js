471a2c09cd333b86f3e537a1a13a79648f2e73c7
Muwahib Abdirahman <muwahib2050@gmail.com>
2022-10-15 12:30:01 -0700

    2nd commit

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,31 @@
 let todobtn = document.querySelector(".todobtn")
-todobtn.addEventListener('click', function(){
-    todolist()
+todobtn.addEventListener('click', function () {
+    todolist();
+
 })
-function todolist(){
+function todolist() {
+    var todo_template = ` <li>
+    <input type="checkbox" id="check" name="accept" onchange="checklist(this.value)"/>Do Homework 
+  </li>`
     var input = document.getElementById("input");
     var todoHolder = document.getElementById("todoholder")
-    var li = document.createElement("li")   
-    if (input.value ==""){
-        alert("ma noqon karto empty")
-    }else{
+    var li = document.createElement("li")
+    var checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    if (input.value == "") {
+        alert("ma madhnan karo")
+    } else {
+        checkbox.innerHTML = input.value;
         li.innerHTML = input.value;
-        todoHolder.appendChild(li);
+        todoHolder.innerHTML += ` <li>
+        <input type="checkbox" id="check" name="accept" onchange="checklist(this.value)" />${input.value}
+      </li>`
         input.value = "";
     }
-} 
+}
 
-
-
-
+function checklist(value){
+    
+    //checked
+    //unchecked
+}
