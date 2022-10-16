2534db04de9eecdd109070ce88164dd33494b5d8
Muwahib Abdirahman <muwahib2050@gmail.com>
2022-10-16 12:01:14 -0700

    3rd commit

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,17 @@
 let todobtn = document.querySelector(".todobtn")
 todobtn.addEventListener('click', function () {
     todolist();
-
+    checklist();
+    delete_check();
+})
+let del_button = document.querySelector(".del_button")
+del_button.addEventListener('click', function () {
+   
+    delete_check();
 })
 function todolist() {
     var todo_template = ` <li>
-    <input type="checkbox" id="check" name="accept" onchange="checklist(this.value)"/>Do Homework 
+    <input type="checkbox" id="check" name="accept" onchange="checklist(this)" />Do Homework 
   </li>`
     var input = document.getElementById("input");
     var todoHolder = document.getElementById("todoholder")
@@ -18,14 +24,28 @@ function todolist() {
         checkbox.innerHTML = input.value;
         li.innerHTML = input.value;
         todoHolder.innerHTML += ` <li>
-        <input type="checkbox" id="check" name="accept" onchange="checklist(this.value)" />${input.value}
-      </li>`
+        <input type="checkbox" id="check" name="accept" onchange="checklist(this)" />${input.value}
+        <i class="fa-regular fa-trash-can"></i></li>`
         input.value = "";
     }
 }
+function checklist(checkbox){ 
 
-function checklist(value){
-    
-    //checked
-    //unchecked
+  if (checkbox.checked) {
+    checkbox.parentElement.classList.add("done");
+  } else {
+    checkbox.parentElement.classList.remove("done");
+  }
 }
+
+function delete_check(){
+    var checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    if (checkbox.checked) {
+        checkbox.parentElement.classList.remove("done");
+      } else {
+        alert("wow");
+      }
+
+}
+
