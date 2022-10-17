669599b741f0d5efb6898e4c6b1ae865d7a669f1
Muwahib Abdirahman <muwahib2050@gmail.com>
2022-10-17 12:23:55 -0700

    4th commit

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,14 @@
 let todobtn = document.querySelector(".todobtn")
 todobtn.addEventListener('click', function () {
     todolist();
-    checklist();
-    delete_check();
+    // checklist();
+    // delete_todo();
 })
-let del_button = document.querySelector(".del_button")
-del_button.addEventListener('click', function () {
-   
-    delete_check();
-})
+
 function todolist() {
     var todo_template = ` <li>
     <input type="checkbox" id="check" name="accept" onchange="checklist(this)" />Do Homework 
-  </li>`
+    <button class="del_button" onclick="delete_todo(this)">Delete</button></li>`
     var input = document.getElementById("input");
     var todoHolder = document.getElementById("todoholder")
     var li = document.createElement("li")
@@ -25,7 +21,8 @@ function todolist() {
         li.innerHTML = input.value;
         todoHolder.innerHTML += ` <li>
         <input type="checkbox" id="check" name="accept" onchange="checklist(this)" />${input.value}
-        <i class="fa-regular fa-trash-can"></i></li>`
+        <button class="del_button" onclick="delete_todo(this)"">Delete</button></li>`
+        localStorage.setItem("todo1", input.value)
         input.value = "";
     }
 }
@@ -38,14 +35,12 @@ function checklist(checkbox){
   }
 }
 
-function delete_check(){
-    var checkbox = document.createElement("input")
-    checkbox.setAttribute("type", "checkbox")
-    if (checkbox.checked) {
-        checkbox.parentElement.classList.remove("done");
-      } else {
-        alert("wow");
-      }
+function delete_todo(del_button){
 
+    if (del_button != "") {
+        // document.querySelector("li").remove();
+        del_button.parentElement.remove();
+      } else{
+        alert("we can't delete a todo which is not done");
+      }    
 }
-
